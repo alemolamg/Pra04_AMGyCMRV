e4135ee34742a887cbcb3f19487168b98373a1bf
Alemol <alemolamg@user.github.com>
2019-11-07 12:05:03 +0100

    ver 1.2.1: EcoCityMoto::CargarMoto funciona correctamente

--- Pra04/EcoCityMoto.cpp
+++ Pra04/EcoCityMoto.cpp
@@ -62,7 +62,7 @@ void EcoCityMoto::cargarMotos(string fileNameMotos){ //ToDo: adaptar para map
                 
                 //con todos los atributos leídos, se crea la moto
                 Moto moto(tipo, matricula, dlat, dlon);
-                //motos.insertar(moto); //ToDo no sirve esto
+                motos.push_back(moto);
                 //comprobacion lectura
                //std::cout << moto.getId() << ";" << moto.getStatus() <<std::endl;            
             }              
